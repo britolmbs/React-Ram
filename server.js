1ccee1d772c4a41fa167de2e5b3ec19dbc01cdd4
britolmbs <britolmbs@hotmail.com>
2022-06-24 10:03:54 -0300

    Projeto finalizado

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const http = require('http');
 const { url } = require('inspector');
 const host = 'http://localhost';
 const port = 3000;
-const stats = './pcRam.js';
+const stats = require('./pcRam');
 
-http.createServer((req, res) => 
-let url = req.url;
-if(url ==)
-res.end('<h1>Working</h1>')).listen(port, () => console.log(`server is running in ${host}:${port}`));
+
+http.createServer((req, res) => {
+    let url = req.url;
+
+    if (url == '/stats'){
+        res.end(JSON.stringify(stats, null, 2));
+    } else {
+        res.end('Seja Bem vindo!')
+    }
+ 
+
+}).listen(port, ()=> console.log(`Server is running in ${host}:${port}`));
